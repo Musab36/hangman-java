ad0114af33d6e7d40dccee07a781275db2079767
Musab36 <musabhd@gmail.com>
2017-08-18 18:47:47 +0300

    Hangman game

--- Hangman.java
+++ Hangman.java
@@ -8,66 +8,71 @@ public class Hangman {
 		String[] guesses = {"moringa", "phones", "cars", "house", "peace", "lake", "mountain", "forest", "love", "happiness", "programming"};
 		
 		boolean letsPlay = true;
-		while(letsPlay) {
-		System.out.println("Welcome to my game of Hangman");
+		while(letsPlay) { // While letsPlay is true, the code in the while loop is run
+		System.out.println("Welcome to my game of Hangman"); // A welcome message
 		
-		char[] randomWords = guesses[random.nextInt(guesses.length)].toCharArray();
-		int amountOfGuesses = randomWords.length;
-		char[] playerGuesses = new char[amountOfGuesses];
+		// We then get random words from the array guesses for the player to guess
+		// .nexInt returns an int from 0 
+		char[] randomWords = guesses[random.nextInt(guesses.length)].toCharArray(); // .toCharArray converts the random word into single characters h,o,u,s,e
+		int amountOfGuesses = randomWords.length; // How many times the player can guess and is set to how long the word is
+		char[] playerGuesses = new char[amountOfGuesses]; // An empty char array that saves the user's input
 		
+		// A for loop initialising the user's char array
 		for(int i = 0; i < playerGuesses.length; i++) {
-			playerGuesses[i] = '_';
+			playerGuesses[i] = '_'; // We are setting each field with '_'
 		}
-		boolean wordIsGuessed = false;
-		int tries = 0;
 		
-		while(!wordIsGuessed && tries != amountOfGuesses) {
-			System.out.println("Current Guesses: ");
-			printArray(playerGuesses);
-			System.out.printf("You have %d tries left.\n", amountOfGuesses - tries);
-			System.out.println("Enter your single character");
-			char input = scanner.nextLine().charAt(0);
-			tries ++;
+		// The beginning of the game
+		boolean wordIsGuessed = false; // We set it to false because at the start the word is not guessed
+		int tries = 0; // Amount of tries
+		
+		while(!wordIsGuessed && tries != amountOfGuesses) { // While the word is not guessed and tries are not equal to amount of guesses
+			System.out.println("Current Guesses: "); // We are printing the current user guesses
+			printArray(playerGuesses); // A function
+			System.out.printf("You have %d tries left.\n", amountOfGuesses - tries); // We give the user their amount of tries
+			System.out.println("Enter your single character"); // Then we prompt the user to enter single characters at a time
+			char input = scanner.nextLine().charAt(0); // This will take only the first character even if the user inputs many characters
+			tries ++; // Then we incriment the user's tries
 			
-			if(input == '-') {
+			if(input == '-') { // If the user inputs '-' we end the game
 				letsPlay = false;
 				wordIsGuessed = true;
-			} else {
+			} else { 
 		for(int i = 0; i < randomWords.length; i++) {
-			if(randomWords[i] == input) {
+			if(randomWords[i] == input) { // If the player inputs other than '-' the input is collected
 				playerGuesses[i] = input;
 			}
 		}
 		
-		if(isTheWordGuessed(playerGuesses)) {
-			wordIsGuessed = true;
+		if(isTheWordGuessed(playerGuesses)) { // If the input is equals to the word guessed
+			wordIsGuessed = true; // The wordIsGuessed is set to true and the user wins
 			System.out.println("Congratulations you have won!");
 			
 		       }
 			}
 		 }
 		 
-		 if(!wordIsGuessed) System.out.println("You ran out of guesses :/");
-		 System.out.println("Do you want to play another game? (yes/no)");
-		 String anotherGame = scanner.nextLine();
+		 if(!wordIsGuessed) System.out.println("You ran out of guesses....."); // If the word is not gussed and the tries left are 0
+		 System.out.println("Do you want to play another game? (yes/no)"); // We prompt the player is they want to retry again
+		 String anotherGame = scanner.nextLine(); // Then we get the user input
 			
-			if(anotherGame.equals("no")) letsPlay = false;
+			if(anotherGame.equals("no")) letsPlay = false; // If the user inputs no
 		}
 		
-		System.out.println("Game Over.....");
+		System.out.println("Game Over....."); // The game is over
 	}
 	
-	public static void printArray(char[] array) {
+	public static void printArray(char[] array) { // A function taking a char array
 		for(int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
 		System.out.println();
 	}
 	
-	public static boolean isTheWordGuessed(char[] array) {
+	public static boolean isTheWordGuessed(char[] array) { // A function taking a char arry
 		for(int i = 0; i < array.length; i++) {
-			if(array[i] == '_') return false;
+			if(array[i] == '_') return false; // If we find '_' that means the word is not guessed
 		}
-		return true;
+		return true; // If we don't find '_' that means the word is guessed
 	}
 }
